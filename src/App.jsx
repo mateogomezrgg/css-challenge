65a4bdd0abf5076087ca45013842bcd201921095
Mateo Gomez <122096768+mateogomezrgg@users.noreply.github.com>
2024-11-26 14:03:32 -0300

    first commit. second row completed.

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='firstRow'>
+        <span className='brutal'>BRUTAL</span>
+        <span className='features'>FEATURES</span>
+        <span className='services'>SERVICES</span>
+        <span className='prices'>PRICES</span>
+        <span className='contact'>CONTACT</span>
+      </div>
+      <div className='secondRow'>
+        <span className='brutalist'>
+          A BRUTALIST <br /> Bootstrap V4 Theme
+        </span>
+        <div className='callToAction__container'>
+          <span className='callToAction'>THERE IS A CALL TO ACTION</span>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
